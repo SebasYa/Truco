8830a337e7d9e804b9a77289d9328c233d7dea4a
Sebastian Yanni <sebasyanni@gmail.com>
2024-07-22 04:22:54 -0300

    Initial Commit

---
+++ AnotadorTruco/AnotadorTrucoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnotadorTrucoApp.swift
+//  AnotadorTruco
+//
+//  Created by Sebastian Yanni on 22/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnotadorTrucoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
